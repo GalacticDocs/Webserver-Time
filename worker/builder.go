7012042c909f737c9016727eaffdf5fae34ea70f
Vitaliya <42318815+iVitaliya@users.noreply.github.com>
2022-07-18 08:15:15 +0000

    Additions

--- worker/builder.go
+++ worker/builder.go
@@ -1,9 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
 type IStringBuilder struct {
-	append func(string) string
-	remove func(string) string
-	replace func(string) string
+	append     func(string) string
+	remove     func(string, int) string
+	replace    func(string, string) string
+	replaceAll func(string, string) string
+	format     func([]string) string
+	toString   func() string
 }
 
-func StringBuilder(original string) *StringBuilder {
+func StringBuilder(original string) *IStringBuilder {
 	var string_val string = original
+
+	value := &IStringBuilder{
+		append: func(value string) string {
+			string_val = original + value
+
+			return string_val
+		},
+		remove: func(to_replace string, amount int) string {
+			string_val = strings.Replace(string_val, to_replace, "", amount)
+
+			return string_val
+		},
+		replace: func(to_replace string, replaceble string) string {
+			string_val = strings.Replace(string_val, to_replace, replaceble, -1)
+
+			return string_val
+		},
+		replaceAll: func(to_replace string, replaceble string) string {
+			string_val = strings.ReplaceAll(string_val, to_replace, replaceble)
+
+			return string_val
+		},
+		format: func(values []string) string {
+			str := FormatString(string_val, values)
+
+			return str
+		},
+		toString: func() string {
+			return string_val
+		},
+	}
+
+	return value
+}
+
+func FormatString(to_format string, values []string) string {
+	var builder = StringBuilder(to_format)
+
+	for i := 0; i < len(values); i++ {
+		var val string = fmt.Sprint(i)
+
+		builder.replace("{"+val+"}", values[i])
+	}
+
+	return builder.toString()
 }
--- worker/time.go
+++ worker/time.go
@@ -1,5 +1,14 @@
 package worker
 
+import "time"
+
+var now = time.Now()
+
 func amOrPm() string {
-	
+	var hour = now.Hour()
+	var builder = StringBuilder("{0} {1}")
+
+	if hour < 12 {
+		
+	}
 }
